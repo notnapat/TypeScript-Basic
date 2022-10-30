04dee822b9613f16659744ea5acd26c15c532eee
notnapat <notnapat9@gmail.com>
2022-10-30 15:06:29 +0700

    Switch-Case คล้ายกัelseif

---
+++ dist/Swtich-Case คล้ายกับ elseif.js
@@ -0,0 +1,18 @@
+"use strict";
+let service = 2;
+let result2;
+switch (service) {
+    case 1:
+        result2 = "1";
+        break;
+    case 2:
+        result2 = "2";
+        break;
+    case 3:
+        result2 = "3";
+        break;
+    default:
+        result2 = "ไม่ตรงเลขได";
+        break;
+}
+console.log(result2);
