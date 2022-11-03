c80703dc3044f35195bcc39fdd3cf4675f646a6e
notnapat <notnapat9@gmail.com>
2022-11-03 19:09:51 +0700

    Tuple == ตัวเก็บข้อมูลแบบ array กับ object คล้ายๆกัน

---
+++ dist/Tuple == ตัวเก็บข้อมูลแบบ array กับ object คล้ายๆกัน.js
@@ -0,0 +1,12 @@
+"use strict";
+// Tuple  == คล้ายๆกับ array แต่ ต้องระบุจำนวน type ชัดเจน และสามารถ มีชนิดข้อมูลของสมาชิกต่างกันได้
+const aaaa3 = [10, 20, "ccc"];
+// เปลี่ยนข้อมูล
+aaaa3[0] = 30;
+console.log(aaaa3[0]);
+console.log(aaaa3);
+// Tule Label == กำหนดชื่อ หรือ กำหนดความหมายให้ แต่ละ type ของข้อมูล
+const days = [11, 22, "33"];
+console.log(days);
+const days2 = ["one", 22222, "three3"];
+console.log(days2);
---
+++ src/Tuple == ตัวเก็บข้อมูลแบบ array กับ object คล้ายๆกัน.ts
@@ -0,0 +1,17 @@
+// Tuple  == คล้ายๆกับ array แต่ ต้องระบุจำนวน type ชัดเจน และสามารถ มีชนิดข้อมูลของสมาชิกต่างกันได้
+const aaaa3:[number,number,string]=[10,20,"ccc"]
+// เปลี่ยนข้อมูล
+aaaa3[0] = 30
+
+console.log(aaaa3[0])
+console.log(aaaa3)
+
+// Tule Label == กำหนดชื่อ หรือ กำหนดความหมายให้ แต่ละ type ของข้อมูล
+const days:[one:number,two:number,three:string] = [11,22,"33"]      
+console.log(days)
+
+// สร้าง Type แบบ Tuple 
+type Alldays  = [string,number,string]
+
+const  days2:Alldays = ["one",22222,"three3"]
+console.log(days2)
